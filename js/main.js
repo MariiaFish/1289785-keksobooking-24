88203622f822db71011017e2effac4604c6dddb1
Mariia <64638931+MariiaFish@users.noreply.github.com>
2021-10-07 15:15:20 +0200

    Refactoring of main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,14 @@ const LNG_MIN = 139.70000;
 const LNG_MAX = 139.80000;
 const LOCATION_FLOAT_VALUE = 5;
 
-const avatarValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const titles = ['Домик в Альпах', 'Дворец в Гилинджике', 'Вилла на берегу Испании', 'Место под палатку на моем заднем дворе', 'Убитая хата для тусовок', 'Уютная комнатка в сердце Амстердама', 'Огромный дом в Провансе', 'Аппартаменты твоей мечты в Праге'];
-const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const timePoints = ['12:00', '13:00', '14:00'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const descriptions = ['Это именно то, что тебе нужно', 'Подходит только для богатых богачей', 'Здесь сбываются мечты', 'Идеально для вечеринок после сдачи сессии', 'Уютно и тепло как у мамы дома', 'Очень близко к звездам', 'Просто взгляните на эти фотки', 'Свежий запах моря'];
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TITLES = ['Домик в Альпах', 'Дворец в Гилинджике', 'Вилла на берегу Испании', 'Место под палатку на моем заднем дворе', 'Убитая хата для тусовок', 'Уютная комнатка в сердце Амстердама', 'Огромный дом в Провансе', 'Аппартаменты твоей мечты в Праге'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME_POINTS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['Это именно то, что тебе нужно', 'Подходит только для богатых богачей', 'Здесь сбываются мечты', 'Идеально для вечеринок после сдачи сессии', 'Уютно и тепло как у мамы дома', 'Очень близко к звездам', 'Просто взгляните на эти фотки', 'Свежий запах моря'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+let avatarNumber = 0;
 
 const getRundomIntNumber = (first, second) => {
   const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
@@ -39,37 +40,46 @@ const getRandomArrayLength = (array, minValue) => {
   return newArray;
 };
 
-const getArrayElement = (array) => {
-  const number = array.shift();
-  return number > 9
-    ? `img/avatars/user${number}.png`
-    : `img/avatars/user0${number}.png`;
+const getAvatarAddress = () => {
+  for (let iteration = avatarNumber + 1; iteration <= 10; iteration++) {
+    avatarNumber++;
+    return avatarNumber > 9
+      ? `img/avatars/user${avatarNumber}.png`
+      : `img/avatars/user0${avatarNumber}.png`;
+  }
 };
 
-const getAd = function () {
-  return {
+const getOfferAddress = (object) =>
+  object.offer.address = `${object.location.lat.toString()}, ${object.location.lng.toString()}`;
+
+const getAd = () => {
+  let ad = {};
+  ad = {
     author: {
-      avatar: getArrayElement(avatarValues),
+      avatar: getAvatarAddress(),
     },
     offer: {
-      title: getRandomArrayElement(titles, MIN),
+      title: getRandomArrayElement(TITLES, MIN),
       address: '',
       price: getRundomIntNumber(MIN, MAX),
-      type: getRandomArrayElement(types, MIN),
+      type: getRandomArrayElement(TYPES, MIN),
       rooms: getRundomIntNumber(MIN, MAX),
       guests: getRundomIntNumber(MIN, MAX),
-      checkin: getRandomArrayElement(timePoints, MIN),
-      checkout: getRandomArrayElement(timePoints, MIN),
-      features: getRandomArrayLength(features, MIN_ARRAY_LENGTH),
-      description: getRandomArrayElement(descriptions, MIN),
-      photos: getRandomArrayLength(photos, MIN_ARRAY_LENGTH),
+      checkin: getRandomArrayElement(TIME_POINTS, MIN),
+      checkout: getRandomArrayElement(TIME_POINTS, MIN),
+      features: getRandomArrayLength(FEATURES, MIN_ARRAY_LENGTH),
+      description: getRandomArrayElement(DESCRIPTIONS, MIN),
+      photos: getRandomArrayLength(PHOTOS, MIN_ARRAY_LENGTH),
     },
     location: {
       lat: getRandomFloatNumber(LAT_MIN, LAT_MAX, LOCATION_FLOAT_VALUE),
       lng: getRandomFloatNumber(LNG_MIN, LNG_MAX, LOCATION_FLOAT_VALUE),
     },
   };
+  getOfferAddress(ad);
+  return ad;
 };
+
 
 const genArraay = () => Array.from({length: 10}, getAd);
 
