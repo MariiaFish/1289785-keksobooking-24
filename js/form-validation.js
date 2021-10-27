6278f25309d8a5da4beac39d88f89a3fb9ded382
Mariia <64638931+MariiaFish@users.noreply.github.com>
2021-10-27 17:04:31 +0200

    Validitation task part 2

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,17 +1,29 @@
 const NUL_VALUE = 0;
-const MIN_CAPACITY_ARRAY_LENGHT = 1;
-const COUNTER_VARIABLE = 1;
 
 const adForm = document.querySelector('.ad-form');
 const roomNumber = adForm.querySelector('#room_number');
 const capacity = adForm.querySelector('#capacity');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const timein = adForm.querySelector('#timein');
+const timeout = adForm.querySelector('#timeout');
 
-const CAPACITY_TYPES = {
+const capacityTypes = {
   1: ['для 1 гостя'],
   2: ['для 2 гостей', 'для 1 гостя'],
   3: ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
   100: ['не для гостей'],
 };
+
+const minFlatPrices = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+const getTargetValue = (evt) => evt.target.options[evt.target.selectedIndex].value;
 
 const createOptionCapacity = (valueAttribute, textContent) => {
   const newOption = document.createElement('option');
@@ -22,16 +34,34 @@ const createOptionCapacity = (valueAttribute, textContent) => {
 
 const validationRoomAndCapacity = (evt) => {
   capacity.innerHTML = '';
-  const capacityTypes = CAPACITY_TYPES[evt.target.value];
-  let valueAttribute = capacityTypes.length;
-  for (let i = COUNTER_VARIABLE; i <= capacityTypes.length; i++) {
-    let newOption = createOptionCapacity(valueAttribute, capacityTypes[i-COUNTER_VARIABLE]);
-    if (capacityTypes.length === MIN_CAPACITY_ARRAY_LENGHT && capacityTypes[NUL_VALUE] === 'не для гостей') {
-      newOption = createOptionCapacity(NUL_VALUE, capacityTypes[NUL_VALUE]);
+  const selectedValue = getTargetValue(evt);
+  const capacityValues = capacityTypes[selectedValue];
+  let valueAttribute = capacityValues.length;
+  capacityValues.forEach((element) => {
+    let newOption = createOptionCapacity(valueAttribute, element);
+    if (element === 'не для гостей') {
+      newOption = createOptionCapacity(NUL_VALUE, element);
     }
     capacity.appendChild(newOption);
     valueAttribute--;
+  });
+};
+
+const changeMinAndPlaceholder = (evt, element) => {
+  const selectedValue = getTargetValue(evt);
+  element.min = minFlatPrices[selectedValue];
+  element.placeholder = minFlatPrices[selectedValue];
+};
+
+
+const moveSelectedAttribute = (evt, selectedMovedElement) => {
+  const selectedValue = getTargetValue(evt);
+  const selectedElements = selectedMovedElement.options;
+  for (const selectedElement of selectedElements) {
+    if (selectedElement.value === selectedValue) {
+      selectedElement.selected = true;
+    }
   }
 };
 
-export { adForm, validationRoomAndCapacity, roomNumber };
+export { adForm, validationRoomAndCapacity, roomNumber, type, price, changeMinAndPlaceholder, timeout, timein, moveSelectedAttribute};
